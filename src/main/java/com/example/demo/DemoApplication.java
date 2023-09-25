6c6ed6f425dd3a333b37b36dc5d194d67d7cdedf
yuzhongyao <60209555+yuzhongyao@users.noreply.github.com>
2023-09-25 01:48:13 -0400

    apply css

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+@Theme("my-theme")
+public class DemoApplication implements AppShellConfigurator {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
