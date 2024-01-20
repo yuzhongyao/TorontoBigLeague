e4f121af28eb5886a24fda777e8cd6456e90af09
yuzhongyao <60209555+yuzhongyao@users.noreply.github.com>
2024-01-20 05:02:01 -0500

    Update DemoApplication.java
    add google sense script to head tag

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,9 @@
 package com.example.demo;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Inline;
+import com.vaadin.flow.component.page.TargetElement;
+import com.vaadin.flow.server.AppShellSettings;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +11,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @Theme("my-theme")
 public class DemoApplication implements AppShellConfigurator {
+
+	@Override
+	public void configurePage(AppShellSettings settings){
+		settings.addInlineFromFile(
+				TargetElement.HEAD, Inline.Position.APPEND,
+				"google.html", Inline.Wrapping.AUTOMATIC
+		);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
