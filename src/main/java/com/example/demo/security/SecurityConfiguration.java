33fa478c44d76c5b046a350ac67dbc39d41adbd1
yuzhongyao <60209555+yuzhongyao@users.noreply.github.com>
2024-09-19 19:58:48 -0400

    Create SecurityConfiguration.java

---
+++ src/main/java/com/example/demo/security/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package com.example.demo.security;
+
+
+import com.example.demo.views.LoginView;
+import com.vaadin.flow.spring.security.VaadinWebSecurity;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends VaadinWebSecurity {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        super.configure(http);
+        setLoginView(http, LoginView.class);
+    }
+}
